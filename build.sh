c562096933ec1638a0c5b41ac1a60bfd7998c15a
Alexey Kachanov <35109763+lexeyOK@users.noreply.github.com>
2022-12-16 23:31:29 +0500

    show build size

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 TARGET=web
 
 cargo fmt
-wasm-pack build --target $TARGET --no-typescript
+wasm-pack build --target $TARGET --no-typescript 
 rm -rf pkg/.gitignore pkg/package.json pkg/README.md
+ls -l pkg/*.wasm
