71ec962fb5d6c29b20b000e55640634e705334da
Alexey Kachanov <35109763+lexeyOK@users.noreply.github.com>
2022-12-18 15:33:50 +0500

    change bliud dir to dist return wee_alloc

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@
 set -euo pipefail
 
 TARGET=web
+BUILD_DIR=dist
 
 cargo fmt
-wasm-pack build --target $TARGET --no-typescript 
-rm -rf pkg/.gitignore pkg/package.json pkg/README.md
-ls -l pkg/*.wasm
+wasm-pack build --target $TARGET --no-typescript -d $BUILD_DIR
+cd $BUILD_DIR
+rm -rf .gitignore package.json README.md
+ls -l *.wasm
+cd ..
