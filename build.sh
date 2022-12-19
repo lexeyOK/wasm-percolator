0da8f11c367b5d5560c9aad705260b84fa996175
Alexey Kachanov <35109763+lexeyOK@users.noreply.github.com>
2022-12-20 00:16:24 +0500

    format build file

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 set -euo pipefail
+
 NAME=wasm_perocator.wasm
 TARGET=wasm32-unknown-unknown
 BINARY=target/$TARGET/release/$NAME
 DIR=dist
+
 cargo build --target $TARGET --release
 wasm-strip $BINARY
+
 mkdir -p $DIR
+
 wasm-opt -o $DIR/$NAME -Oz $BINARY
+
 ls -l $DIR/$NAME
